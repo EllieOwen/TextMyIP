30a14d10473c3af30a8014821fff828e34f1eeff
EllieOwen <leonschmidt3.14@gmail.com>
2016-11-27 20:52:51 -0500

    fix pointer, add config template

---
+++ configtemplate.h
@@ -0,0 +1,15 @@
+/*
+ * Put your own parameters in this and save as config.h
+ * This is an example of sending a text message via ATT 
+ * from a gmail account
+*/
+
+/*Email being sent too: */
+#define TO "1234567890@txt.att.net"
+#define FROM "sender@email.com"
+#define CC "carboncopy@email.com"
+
+/*Account being sent from:  */
+#define USERNAME "mylogin@gmail.com"
+#define PASSWORD "password"
+#define URL "smtps://smtp.gmail.com:465"
